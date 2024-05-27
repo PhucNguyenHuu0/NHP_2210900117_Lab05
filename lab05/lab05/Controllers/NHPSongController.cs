9b88dfdda69ac0d3f84dcd8cba40db23b5b9a3f7
Nguyễn Hữu Phúc <nguyenphuc10122003@gmail.com>
2024-05-27 21:00:48 +0700

    lab05_ngay27052024

---
+++ lab05/lab05/Controllers/NHPSongController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab05.Models;
+
+namespace lab05.Controllers
+{
+    public class NHPSongController : Controller
+    {
+        private static List<NHPSong> NHPSongs = new List<NHPSong>()
+        {
+            new NHPSong{id=221,nhpTitle = "Nguyen Huu Phuc",nhpAuthor = "K22CNT1,",nhpArtist="Phuc Phuc",nhpYaerRelease=2024},
+            new NHPSong{id=2222,nhpTitle = "Nguyen Tien Loc",nhpAuthor = "K22CNT1,",nhpArtist="Loc",nhpYaerRelease=2024},
+        };
+        // GET: NHPSong
+        public ActionResult Index()
+        {
+            return View(NHPSongs);
+        }
+        // get: create song
+        public ActionResult NHPCreate()
+        {
+            var NHPSong = new NHPSong();
+
+            return View(NHPSong);
+        }
+        // Post: create song
+        [HttpPost]
+        public ActionResult NHPCreate(NHPSong NHPSong)
+        {
+            if (!ModelState.IsValid) // neu loi xay ra
+            {
+                return View(NHPSong);
+            }
+            // neu du lieu dung -> luu du lieu vao danh sach bai hat
+            NHPSongs.Add(NHPSong);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ lab05/lab05/Models/NHPSong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace lab05.Models
+{
+    public class NHPSong
+    {
+        public int id { get; set; }
+        [DisplayName("tieu de")]
+        [Required(ErrorMessage = "NHP:hay nhap tieu de")]
+        public string nhpTitle { get; set; }
+        [DisplayName("tac gia")]
+        [Required(ErrorMessage = "NHP:hay nhap tac gia")]
+        public string nhpAuthor { get; set; }
+        [DisplayName("nghe si")]
+        [Required(ErrorMessage = "NHP:hay nhap nghe si")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "NHP: dai phai trong khoang[2-50]")]
+        public string nhpArtist { get; set; }
+        [DisplayName("nam xuat ban")]
+        [Required(ErrorMessage = "NHP:hay nhap nam xuat ban")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "NHP: nam xuat ban phai la so co ki tu" )]
+        [Range(1900,2020, ErrorMessage ="NHP nam xuat ban trong khoang 1900 - 2020")]
+        public int nhpYaerRelease { get; set; }
+        
+    }
+}
